29abf680bd1c793ec7b55ba15f95dd81934a08d1
Arin Muni Bajracharya <arinbajracharya9@gmail.com>
2026-05-23 11:01:05 +0545

    first commit

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useEffect, useState } from "react";
+
+ const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme || "light";
+  });
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
