8fb54cd370513f3cdc610b1938faecc1f133b5d9
Arin Muni Bajracharya <arinbajracharya9@gmail.com>
2026-05-23 11:48:36 +0545

    Create protectedRoute function

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/context";
 
-export const useAuth = () => {
+export default function useAuth () {
   const context = useContext(AuthContext);
   if (!context) {
     throw new Error("useAuth must be used within a AuthProvider");
