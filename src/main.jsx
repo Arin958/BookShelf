c9cc74cf0bf028115674b1eb230d6ebeeb1d4572
Arin Muni Bajracharya <arinbajracharya9@gmail.com>
2026-05-23 12:01:23 +0545

    Add browserRouter nad Authprovider in main file

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import ThemeProvider from "./context/ThemeContext.jsx";
+import AuthProvider from "./context/AuthContext.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
