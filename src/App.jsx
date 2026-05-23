648978d5238eb20434a688ec4ee3070a7c767eb7
Arin Muni Bajracharya <arinbajracharya9@gmail.com>
2026-05-23 12:11:55 +0545

    Add protectedRoute in shelf page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/Login";
 import ShelfPage from "./pages/Shelf";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/shelf" element={<ShelfPage />} />
+        <Route
+          path="/shelf"
+          element={
+            <ProtectedRoute>
+              <ShelfPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </>
   );
