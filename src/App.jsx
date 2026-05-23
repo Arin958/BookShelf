ee9a96183a66bd38c65ce33a39634eb674f86cbf
Arin Muni Bajracharya <arinbajracharya9@gmail.com>
2026-05-23 12:11:20 +0545

    Add shelf page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import LoginPage from './pages/Login'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import LoginPage from "./pages/Login";
+import ShelfPage from "./pages/Shelf";
 
 function App() {
-
-
   return (
     <>
-     <Routes>
-      <Route path="/" element={<LoginPage />} />
-     </Routes>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/shelf" element={<ShelfPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Shelf.jsx
@@ -0,0 +1,9 @@
+import Shelf from "../components/Shelf/Shelf";
+
+export default function ShelfPage() {
+    return (
+        <>
+            <Shelf />
+        </>
+    );
+}
