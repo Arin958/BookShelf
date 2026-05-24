3338648c21891e258b4eed8a83bee63147c0dfd7
Arin Muni Bajracharya <arinbajracharya9@gmail.com>
2026-05-24 20:55:41 +0545

    update store for redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import booksReducer from "./slices/bookSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "books",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, booksReducer);
 
 export const store = configureStore({
-    reducer: {
-        books: booksReducer
-    },
-})
+  reducer: {
+    books: persistedReducer,
+  },
+});
+
+export const persistor = persistStore(store);
