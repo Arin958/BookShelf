2b0d7cd88b08214183e97663dada9cb7ec0df6a5
Arin Muni Bajracharya <arinbajracharya9@gmail.com>
2026-05-24 20:53:54 +0545

    add persist in main

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,13 @@ import ThemeProvider from "./context/ThemeContext.jsx";
 import AuthProvider from "./context/AuthContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store.js";
+import { persistor, store } from "./store/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
+      <PersistGate loading={<p>Loading</p>} persistor={persistor}></PersistGate>
       <ThemeProvider>
         <AuthProvider>
           <BrowserRouter>
